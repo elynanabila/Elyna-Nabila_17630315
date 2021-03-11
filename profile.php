cb2f9a3ab6d8c32a31e65e4e44dd66f537cf972d
elynanabila <elynanabila1@gmail.com>
2021-03-11 15:02:56 +0800

    commit pertama: project praktikum Elyna Nabila - 17630315

---
+++ profile.php
@@ -0,0 +1,52 @@
+<?php ?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/elyna.jpg" alt="Elyna Nabila" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIM :</strong></td>
+                        <td align="left" width="50%">17630315</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Elyna Nabila</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 09 Agustus 1999</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0853 8902 9150</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">elynanabila1@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                           
+                            <a href="https://www.instagram.com/elynaa___/" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/029-instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="https://twitter.com/ggukiewaist" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/008-twitter.svg" width="30" alt="twitter">
+                            </a>
+                            
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php include "footer.php" ?>
---
+++ user/user_update.php
@@ -0,0 +1,4 @@
+<?php
+ 
+if (isset($_POST['update'])) {
+    $id = $_POST['id'];
